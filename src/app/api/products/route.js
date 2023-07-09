82806f06fb5f2ac9224c71e683cfad3c0a990cce
manasaggrawal <manas@studiographene.com>
2023-07-09 16:57:19 +0530

    fixed route problem

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,23 +1,25 @@
 import { createProduct, deleteProduct, getAllProducts } from "../../../../prisma/product";
 import { NextResponse } from 'next/server';
+import { parse } from 'url';
 
-
+export async function DELETE(req, res) {
+    
+    const { query } = parse(req.url, true);
+        const id = query.id;
+    console.log('-------printed id here-------',id);
+        await deleteProduct(id);
+    return NextResponse.json({ message: "product deleted" });
+}
 
 export async function POST(req, res) {
     const { image, title, price, category } = await req.json();
-    // console.log("reqbosy",image, title, price, category);
+   
     const new_product = await createProduct(image, title, price, category);
-    // return NextResponse.json({ image, title, price, category });
+    
     return NextResponse.json(req.body);
 }
 
-// export async function GET(req, res) {
-//     const products = await getAllProducts();
-//     return NextResponse.json(products);
-// }
-export async function DELETE(req, res) {
-    const request = await req.json();
-    console.log(request);
-        await deleteProduct(id);
-    return NextResponse.json({ message: "product deleted" });
+export async function GET(req, res) {
+    const products = await getAllProducts();
+    return NextResponse.json(products);
 }
